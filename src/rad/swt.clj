eb0e93535627a6f77fab69901a8e2ca669e6cc2e
Martin Josefsson <josefsson.martin@gmail.com>
2015-06-24 22:11:57 +0200

    Run SWT frontend in separate thread - kind of works

--- src/rad/swt.clj
+++ src/rad/swt.clj
@@ -24,10 +24,40 @@
           (.sleep display))
         (recur)))))
 
+;; Save the 'display' object, so that it is accessible from the repl & more
+(def display-ref (ref nil))
+
 (defn begin []
-  (let [display (Display.)
-        shell (Shell. display)]
-    (create-shell display shell)
-    (.setSize shell 700 700)
-    (.open shell)
-    (swt-loop display shell)))
+  (let [display (Display.)]
+
+    ;; We will want to modify the SWT display object later, so we save it. It's Java
+    (dosync
+     (ref-set display-ref display))
+
+    (let [shell (Shell. display)]
+      (create-shell display shell)
+      (.setSize shell 700 700)
+      (.open shell)
+      (swt-loop display shell))))
+
+;; Credit for this code got to Kevin Albrecht. Thank you! (or tack så mycket!)
+(defn start-gui-threaded []
+  "Launch the GUI in a new thread"
+  (let [thread (Thread. begin)]
+    (.start thread)
+    thread))
+
+;; This macro executes its body in the thread above. It simplifies REPL debugging.
+(defmacro exec [& body]
+  "Run `body` inside the thread of display-ref"
+  `(.syncExec (deref display-ref)
+              (reify Runnable
+                (run [this]
+                  ~@body))))
+
+(defmacro pexec [& body]
+  "Print the result of `body` inside the thread of display-ref"
+  `(.syncExec (deref display-ref)
+              (reify Runnable
+                (run [this]
+                  (println ~@body)))))
