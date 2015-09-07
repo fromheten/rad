80b23e60b34ada28e2bd82b5b9fdcf726baebb17
Martin Josefsson <hello@martinjosefsson.com>
2015-09-07 21:49:04 +0200

    Move insert-mode-handle-keypress! into rad.mode

--- src/rad/core.clj
+++ src/rad/core.clj
@@ -14,33 +14,11 @@
     (not (nil? (re-matches #"^[0-9a-zA-Z ]+$" (str char))))
     false))
 
-(defn insert-mode-handle-keypress!
-  "Takes a key press, and delegates it into the proper action
-
-  Todos:
-  * Make it front-end agnostic - now it depends on many things from the terminal front end"
-  [key] (do
-          (condp = key
-            :enter (do
-                     (buffer/insert-char! @rad.point/point \newline)
-                     ;; move point to one line down x=0, y=y+1
-                     (reset! rad.point/point [0 (+ 1 (second @rad.point/point))])
-                     (rad.point/sync-frontend-cursor-to-point-atom!))
-            :backspace (do
-                         (buffer/delete-char-backwards! @rad.point/point)
-                         (rad.point/move-point-backwards! 1))
-
-            ;; else
-            (do  (buffer/insert-char! @rad.point/point key)
-                 (rad.point/move-point-forward! 1)))
-
-          (println (str "keypress: " key ", point: " @rad.point/point))
-          (terminal/render-buffer! @buffer/current-buffer terminal/scr)))
 
 (defn handle-keypress! [key]
   (if (= :command @rad.mode/current-mode)
     (rad.mode/command-mode-handle-key! key)
-    (insert-mode-handle-keypress! key)))
+    (rad.mode/insert-mode-handle-keypress! key)))
 
 (defn -main []
   (do
--- src/rad/mode.clj
+++ src/rad/mode.clj
@@ -1,7 +1,8 @@
 (ns rad.mode
   "State and functions for entering/leaving modes such as :insert and :command"
   (:require [rad.buffer]
-            [rad.terminal]))
+            [rad.terminal]
+            [rad.point]))
 
 ;; This works only in repl right now ;)
 
@@ -44,6 +45,29 @@
       (reset! current-command-state (command))))
   (println (str "keypress: " key))
   (rad.terminal/render-buffer! @rad.buffer/current-buffer rad.terminal/scr))
+
+(defn insert-mode-handle-keypress!
+  "Takes a key press, and delegates it into the proper action
+
+  Todos:
+  * Make it front-end agnostic - now it depends on many things from the terminal front end"
+  [key] (do
+          (condp = key
+            :enter (do
+                     (rad.buffer/insert-char! @rad.point/point \newline)
+                     ;; move point to one line down x=0, y=y+1
+                     (reset! rad.point/point [0 (+ 1 (second @rad.point/point))])
+                     (rad.point/sync-frontend-cursor-to-point-atom!))
+            :backspace (do
+                         (rad.buffer/delete-char-backwards! @rad.point/point)
+                         (rad.point/move-point-backwards! 1))
+
+            ;; else
+            (do  (rad.buffer/insert-char! @rad.point/point key)
+                 (rad.point/move-point-forward! 1)))
+
+          (println (str "keypress: " key ", point: " @rad.point/point))
+          (rad.terminal/render-buffer! @rad.buffer/current-buffer rad.terminal/scr)))
 
 ;; (deref current-command-state)
 ;; (handle-key! :r)
