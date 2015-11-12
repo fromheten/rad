df8009316883ce9eb65fa4c94386232901732bea
Martin Josefsson <hello@martinjosefsson.com>
2015-11-12 18:58:33 +0100

    Rebooting

--- doc/intro.md
+++
@@ -1,3 +0,0 @@
-# Introduction to rad
-
-TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
--- test/rad/terminal_test.clj
+++
@@ -1,3 +0,0 @@
-(ns rad.terminal-test
-  (:require [clojure.test :refer :all]
-            [rad.terminal :refer :all]))
