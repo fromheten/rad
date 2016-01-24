5065cf7a985dabaa5ef75c0bd4a550d47ceade07
Martin Josefsson <hello@martinjosefsson.com>
2016-01-24 10:49:54 +0100

    Better naming of keys in FX input event

--- test/rad/frontend/fx_test.clj
+++ test/rad/frontend/fx_test.clj
@@ -19,64 +19,64 @@
                                   :to-string "KeyEvent [source = javafx.scene.Scene@4c32ba1c, target = javafx.scene.Scene@4c32ba1c, eventType = KEY_PRESSED, consumed = false, character = , text = A, code = A, shiftDown]"
                                   :character ""
                                   :alt-down? false
-                                  :is-control-down false
+                                  :control-down? false
                                   :is-meta-down false
-                                  :is-shift-down false
-                                  :is-shortcut-down false})))
+                                  :shift-down? false
+                                  :shortcut-down? false})))
     (is (= \A
            (fx-keydown->rad-char {:text "A"
                                   :to-string "KeyEvent [source = javafx.scene.Scene@4c32ba1c, target = javafx.scene.Scene@4c32ba1c, eventType = KEY_PRESSED, consumed = false, character = , text = A, code = A, shiftDown]"
                                   :character ""
                                   :alt-down? false
-                                  :is-control-down false
+                                  :control-down? false
                                   :is-meta-down false
-                                  :is-shift-down true
-                                  :is-shortcut-down false})))
+                                  :shift-down? true
+                                  :shortcut-down? false})))
     (is (= :tab
            (fx-keydown->rad-char {:text "\t"
                                   :to-string "KeyEvent [source = javafx.scene.Scene@b1a43d2, target = javafx.scene.Scene@b1a43d2, eventType = KEY_PRESSED, consumed = false, character = , text = \t, code = TAB]"
                                   :character ""
                                   :alt-down? false
-                                  :is-control-down true
+                                  :control-down? true
                                   :is-meta-down false
-                                  :is-shift-down true
-                                  :is-shortcut-down true})))
+                                  :shift-down? true
+                                  :shortcut-down? true})))
     (is (= \space
            (fx-keydown->rad-char {:text " "
                                   :to-string "KeyEvent [source = javafx.scene.Scene@b1a43d2, target = javafx.scene.Scene@b1a43d2, eventType = KEY_PRESSED, consumed = false, character = , text =  , code = SPACE]"
                                   :character ""
                                   :alt-down? true
-                                  :is-control-down false
-                                  :is-meta-down false
-                                  :is-shift-down false
-                                  :is-shortcut-down false})))
+                                  :control-down? false
+                                  :meta-down? false
+                                  :shift-down? false
+                                  :shortcut-down? false})))
     (is (= :back_space
            (fx-keydown->rad-char {:text ""
                                   :to-string "KeyEvent [source = javafx.scene.Scene@1864ff48, target = javafx.scene.Scene@1864ff48, eventType = KEY_PRESSED, consumed = false, character = , text = , code = BACK_SPACE]"
                                   :character ""
                                   :code "BACK_SPACE"
                                   :alt-down? false
-                                  :is-control-down false
-                                  :is-meta-down false
-                                  :is-shift-down true
-                                  :is-shortcut-down true})))
+                                  :control-down? false
+                                  :meta-down? false
+                                  :shift-down? true
+                                  :shortcut-down? true})))
     (is (= :enter
            (fx-keydown->rad-char {:text ""
                                   :to-string "KeyEvent [source = javafx.scene.Scene@67951658, , code = ENTER]"
                                   :character ""
                                   :code "ENTER"
                                   :alt-down? false
-                                  :is-control-down false
-                                  :is-meta-down false
-                                  :is-shift-down false
-                                  :is-shortcut-down false})))
+                                  :control-down? false
+                                  :meta-down? false
+                                  :shift-down? false
+                                  :shortcut-down? false})))
     (is (= :shift
            (fx-keydown->rad-char {:text ""
                                   :to-string "KeyEvent [source = javafx.scene.Scene@1b4e4710, target = javafx.scene.Scene@1b4e4710, eventType = KEY_PRESSED, consumed = false, character = , text = , code = SHIFT, shiftDown]"
                                   :character ""
                                   :code "SHIFT"
                                   :alt-down? false
-                                  :is-control-down false
-                                  :is-meta-down false
-                                  :is-shift-down true
-                                  :is-shortcut-down false})))))
+                                  :control-down? false
+                                  :meta-down? false
+                                  :shift-down? true
+                                  :shortcut-down? false})))))
